475d18ba1fbd202144f6bfa3a5e43a49eaf15e72
Ctracey92 <cody.tracey92@gmail.com>
2020-05-12 22:34:32 -0500

    added in some dummy data to work with, made variables more DRY, added in teh lvl func

--- src/Pages/Home/home.js
+++ src/Pages/Home/home.js
@@ -8,38 +8,89 @@ class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      str: 4,
-      dex: 10,
-      con: 10,
-      wis: 10,
-      int: 10,
-      cha: 10,
+      char: {
+        username: "Honorius",
+        levels: [
+          "Warlock",
+          "Warlock",
+          "Warlock",
+          "Warlock",
+          "Warlock",
+          "Sorcerer",
+          "Sorcerer",
+          "Sorcerer",
+          "Sorcerer",
+        ],
+        stats: {
+          str: 4,
+          dex: 10,
+          con: 10,
+          wis: 10,
+          int: 10,
+          cha: 10,
+        },
+      },
     };
   }
 
   render() {
+    const modifier = (num) => Math.floor((num - 10) / 2);
+    const character = this.state.char;
+    let lvls = character.levels
+      .reduce((sum, i) => {
+        if (sum.length > 0) {
+          let includes = false;
+          sum.map((item, index) => {
+            if (item.includes(i)) {
+              sum[index][1]++;
+              includes = true;
+            }
+          });
+          if (!includes) {
+            sum.push([i, 1]);
+          }
+        } else {
+          sum.push([i, 1]);
+        }
+
+        return sum;
+      }, [])
+      .map((i) => i.join(": "))
+      .join("|");
+
+    
     return (
       <div>
         <Header />
         <Container>
           <Row>
-            <Col>
-              <h1>Stats</h1>
-              <h3>STR: {this.state.str}</h3>
-              <h3>DEX: {this.state.dex}</h3>
-              <h3>CON: {this.state.con}</h3>
-              <h3>WIS: {this.state.wis}</h3>
-              <h3>INT: {this.state.int}</h3>
-              <h3>CHA: {this.state.cha}</h3>
+            <Col lg={3}>
+              <h4>Username: {character.username}</h4>
             </Col>
-            <Col>
+            <Col lg={9}>
+              <h4>
+                <b>Levels:</b> {lvls}
+              </h4>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={2}>
+              <h1>Stats</h1>
+              <h5>STR: {character.stats.str}</h5>
+              <h5>DEX: {character.stats.dex}</h5>
+              <h5>CON: {character.stats.con}</h5>
+              <h5>WIS: {character.stats.wis}</h5>
+              <h5>INT: {character.stats.int}</h5>
+              <h5>CHA: {character.stats.cha}</h5>
+            </Col>
+            <Col lg={2}>
               <h1>Modifiers</h1>
-              <h3>STR: {Math.floor((this.state.str-10)/2)}</h3>
-              <h3>DEX: {Math.floor((this.state.dex-10)/2)}</h3>
-              <h3>CON: {Math.floor((this.state.con-10)/2)}</h3>
-              <h3>WIS: {Math.floor((this.state.wis-10)/2)}</h3>
-              <h3>INT: {Math.floor((this.state.int-10)/2)}</h3>
-              <h3>CHA: {Math.floor((this.state.cha-10)/2)}</h3>
+              <h5>STR: {modifier(character.stats.str)}</h5>
+              <h5>DEX: {modifier(character.stats.dex)}</h5>
+              <h5>CON: {modifier(character.stats.con)}</h5>
+              <h5>WIS: {modifier(character.stats.wis)}</h5>
+              <h5>INT: {modifier(character.stats.int)}</h5>
+              <h5>CHA: {modifier(character.stats.cha)}</h5>
             </Col>
           </Row>
         </Container>
