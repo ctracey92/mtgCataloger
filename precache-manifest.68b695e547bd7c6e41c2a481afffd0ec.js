b71ba611331851e346b14a3c1f322187b34cd6ac
Ctracey92 <cody.tracey92@gmail.com>
2020-05-15 15:40:15 -0500

    Updates

---
+++ precache-manifest.68b695e547bd7c6e41c2a481afffd0ec.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71b0b73e6fdc0bb997c39ee3fe240662",
+    "url": "/mtgCataloger/index.html"
+  },
+  {
+    "revision": "6773443aa2e35f1c00f7",
+    "url": "/mtgCataloger/static/css/2.9337ff71.chunk.css"
+  },
+  {
+    "revision": "f66308452bd27036609b",
+    "url": "/mtgCataloger/static/css/main.72c36725.chunk.css"
+  },
+  {
+    "revision": "6773443aa2e35f1c00f7",
+    "url": "/mtgCataloger/static/js/2.9619b312.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/mtgCataloger/static/js/2.9619b312.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f66308452bd27036609b",
+    "url": "/mtgCataloger/static/js/main.93afae3c.chunk.js"
+  },
+  {
+    "revision": "eb9c608fb0ee5e9815d5",
+    "url": "/mtgCataloger/static/js/runtime-main.a8138da1.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/mtgCataloger/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/mtgCataloger/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/mtgCataloger/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/mtgCataloger/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/mtgCataloger/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
